73ae0e9580dd32af79b63dc9616bac3174d7ebb7
leandro-gft <lpsacchi@gmail.com>
2020-05-29 17:13:01 -0300

    Adicionados elementos de interface

--- app/src/main/java/br/com/gft/mediaplayer/MainActivity.java
+++ app/src/main/java/br/com/gft/mediaplayer/MainActivity.java
@@ -2,13 +2,21 @@ package br.com.gft.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.chromatica);
+
     }
 }
